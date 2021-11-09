f9331c55532ead1974ac02e31873f1cf2ff76a11
Yakov Chibizov <chibizov96@gmail.com>
2021-11-09 19:01:27 +0300

    Revert "Task 14 , v 12"
    This reverts commit 0f46328b382faf7a89434fa3345c90e53f552ca2.

--- HW/14/Program.cs
+++ HW/14/Program.cs
@@ -1,11 +1,6 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет
-int num = new Random().Next(-100000, 100000);
+int number = new Random().Next(1, 100000);
 Console.WriteLine("Ищем третью справа цифру (количество сотен)");
-void Method(int number)
-{
-    Console.WriteLine("Рандомное число с отрезка: " + number);
-    number = Math.Abs(number);
-    if (number < 100) Console.WriteLine("Третья цифра отсутствует");
-    else Console.WriteLine("Третья справа цифра в числе: " + (number / 100 % 10));
-}
-Method(num);
+Console.WriteLine("Рандомное число с отрезка: " + number);
+if (number < 100) Console.WriteLine("Третья цифра отсутствует");
+else Console.WriteLine("Третья справа цифра в числе: " + (number / 100 % 10));
