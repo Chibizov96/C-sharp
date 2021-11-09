2b1deb3b0894c0c925162ecec0c7cb36d78cf9ff
Yakov Chibizov <chibizov96@gmail.com>
2021-11-09 18:57:16 +0300

    v. 2
    Math.Abs added

--- HW/14/Program.cs
+++ HW/14/Program.cs
@@ -1,9 +1,10 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет
-int num = new Random().Next(1, 100000);
+int num = new Random().Next(-100000, 100000);
 Console.WriteLine("Ищем третью справа цифру (количество сотен)");
 void Method(int number)
 {
     Console.WriteLine("Рандомное число с отрезка: " + number);
+    number = Math.Abs(number);
     if (number < 100) Console.WriteLine("Третья цифра отсутствует");
     else Console.WriteLine("Третья справа цифра в числе: " + (number / 100 % 10));
 }
